853dc072332b60364513f2568296962879f454dc
Micah Young <myoung@pivotal.io>
2017-11-20 06:54:46 -0500

    Step 1

---
+++ step_1.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+cat > src/main/java/com/example/demo/DemoApplication.java <<END
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableCaching
+public class DemoApplication {
+
+    @Bean
+    CachedBigData bigData() {
+        return new CachedBigData();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
+END
+
+cat > src/main/java/com/example/demo/MathController.java <<END
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+    @Autowired
+    IBigData bigData;
+
+    @RequestMapping("/")
+    public String get() {
+        int candidate = 1_234_567_891;
+        return candidate + " = " + bigData.isPrime(candidate);
+    }
+}
+END
+
+cat > src/main/java/com/example/demo/CachedBigData.java <<END
+package com.example.demo;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CachedBigData implements IBigData {
+
+    @Cacheable(value="isPrime")
+    public boolean isPrime(int candidate) {
+        for(int i = 2; i < candidate; i++){
+            if(candidate % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+END
+
+cat > src/main/java/com/example/demo/IBigData.java <<END
+package com.example.demo;
+
+public interface IBigData {
+
+    boolean isPrime(int candidate);
+
+}
+END
+
+read  -n 1 -p "Continue: " mainmenuinput
+
+set -x
+
+mvn clean verify package -DskipTests
+
+cf push cache-demo -p target/demo-0.0.1-SNAPSHOT.jar
